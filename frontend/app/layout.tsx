da20e7d5f4c5c4200d5a9321a4d3572d1e879614
Edmilson Gandra <edmilsongandra10@gmail.com>
2025-03-08 21:08:03 -0300

    feat(frontend): add login page, mode toggle component, and update styles

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/app/_components/theme-provider';
 import { Toaster } from './_components/ui/sonner';
+import { ModeToggle } from './_components/ModeToggle';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <ModeToggle />
           {children}
           <Toaster />
         </ThemeProvider>
